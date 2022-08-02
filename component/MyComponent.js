139112996b356e1ea3d6f47a2785237cb3797ac9
wzhaofei <wangzf@wellnkiot.com>
2022-08-02 09:53:13 +0800

    chore: 整理代码

--- component/MyComponent.js
+++ component/MyComponent.js
@@ -1,9 +1,18 @@
 import { Component, createElement } from "../lib"
 
 export class MyComponent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            a: 1,
+            b: 2
+        }
+    }
     render() {
         return <div>
             <h1>My Component</h1>
+            <span>{this.state.a.toString()}</span>
+            <span>{this.state.b.toString()}</span>
             {this.children}
         </div>
     }
