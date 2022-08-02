aa03146ff19ad96b64719edf35394163eb845478
wzhaofei <wangzf@wellnkiot.com>
2022-08-02 09:49:25 +0800

    chore: 整理代码

--- component/MyComponent.js
+++ component/MyComponent.js
@@ -2,6 +2,9 @@ import { Component, createElement } from "../lib"
 
 export class MyComponent extends Component {
     render() {
-        return <div><h1>My Component</h1>{this.children}</div>
+        return <div>
+            <h1>My Component</h1>
+            {this.children}
+        </div>
     }
 }
